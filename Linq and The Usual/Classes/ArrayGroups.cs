ae8ef0332e47c62602e72f8432b1e089a6f5f626
Purpel Lizrd <vladislav.nekrasov123@gmail.com>
2022-12-09 12:52:34 +0300

    Add project files.

---
+++ Linq and The Usual/Classes/ArrayGroups.cs
@@ -0,0 +1,53 @@
+﻿namespace Linq_and_The_Usual.Classes
+{
+    public class ArrayGroups
+    {
+        public int[] EvenNumbers { get; private set; }
+        public int[] OddNumbers { get; private set; }
+
+        public int EvenSum { get; private set; }
+        public int OddSum { get; private set; }
+
+        public ArrayGroups(int[] arr)
+        {
+            int[] evenNumbers = new int[arr.Length];
+            int[] oddNumbers = new int[arr.Length];
+
+            int n = 0, f = 0;
+            for (int i = 0; i < arr?.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    evenNumbers[n] = arr[i];
+                    n++;
+                }
+                else
+                {
+                    oddNumbers[f] = arr[i];
+                    f++;
+                }
+            }
+
+            Array.Resize(ref evenNumbers, n);
+            Array.Resize(ref oddNumbers, f);
+
+            EvenNumbers = evenNumbers;
+            OddNumbers = oddNumbers;
+
+            EvenSum = Sum(EvenNumbers);
+            OddSum = Sum(OddNumbers);
+        }
+
+        private static int Sum(int[] arr)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Linq and The Usual/Program.cs
@@ -0,0 +1,70 @@
+﻿using Linq_and_The_Usual.Classes;
+using System;
+using System.Linq;
+
+namespace _12._3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sex = new int[] { 1, 10, 123, 4, 3, 34, 48, 100, 11, 5, 8, 16, 27 };
+            ArrayGroups arr = new ArrayGroups(sex);
+            Console.WriteLine();
+
+            Console.WriteLine("Initial array: ");
+            for (int i = 0; i < sex.Length; i++)
+            {
+                Console.Write(sex[i] + " ");
+            }
+            Console.WriteLine("\n");
+
+            #region Ordinary
+            Console.WriteLine("New groups (Ordinary way):");
+            Console.WriteLine("Even Numbers(" + arr.EvenSum + "): ");
+            for (int i = 0; i < arr.EvenNumbers.Length; i++)
+            {
+                Console.Write(arr.EvenNumbers[i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Odd Numbers(" + arr.OddSum + "): ");
+            for (int i = 0; i < arr.OddNumbers.Length; i++)
+            {
+                Console.Write(arr.OddNumbers[i] + " ");
+            }
+            Console.WriteLine();
+            #endregion
+
+            #region LINQ
+
+            /*var Output = sex.GroupBy(
+            number => number = number ,
+            number => number % 2 != 0,
+            (even, odd) => new
+            {
+            Odd = odd,
+            Even = even
+            });*/
+
+            /*var Output = from element in sex
+            group element by element % 2 == 0;*/
+
+            /*foreach(var item in Output)
+            {
+            //Console.WriteLine("Even numbers: ");
+            foreach (var element in item)
+            {
+
+            }
+
+            }*/
+
+
+            #endregion
+
+
+        }
+    }
+}
